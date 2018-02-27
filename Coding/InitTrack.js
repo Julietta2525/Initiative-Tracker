56fbb83e0fc9ae84ec0f400e5f5803f7c0ff266f
Julietta <hypatia1186@gmail.com>
2018-02-27 16:29:49 -0600

    hgnnnnnnnnnngh (js)

--- Coding/InitTrack.js
+++ Coding/InitTrack.js
@@ -1,6 +1,15 @@
-var x
-var Im = document.getElementById(x)
 
-function hoverImg(x) {
-    document.getElementById(x)
+function newPlayerButton () {
+    //var newPlayerCardData = document.getElementById("charCard")
+    var divSeg = document.createElement("div")
+    var newPlayer = document.getElementsByTagName
+    var att = document.createAttribute("class")
+    att.value = "characterCard"
 }
+
+
+
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_todo
+//https://gomakethings.com/adding-a-new-element-to-the-dom-with-vanilla-js/
+//https://www.digitalocean.com/community/tutorials/how-to-make-changes-to-the-dom
+//http://www.javascriptkit.com/javatutors/dom2.shtml
